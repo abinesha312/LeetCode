8c4bc85fbf7910be8704ab1494f5092feb34058d
Abinesha312 <Abinesha312@gmail.com>
2025-03-26 16:06:44 -0500

    Max Profit new approach with  Brute force | TC - O(n), SC - O(1)

--- Arrays & Hashing/123_StockSellBest.py
+++ Arrays & Hashing/123_StockSellBest.py
@@ -1,25 +1,51 @@
 class solution:
     '''
-        Diffculty leve : Easy
-        Best time to sell stock value.
+    Technique : Two Pointer
+    Time Complexity : O(n)
+    Space Complexity : O(1)
+    Remarks: Make significant improvement
     '''
-    def SellStock(int,prices:list[int])->list[int]:
+    def SellStock_Tp(int,prices:list[int])->list[int]:
         l,r = 0,1
-        maxVa = 0
+        mp = 0
         while r < len(prices):
             if prices[l] < prices[r]:
-                profit = prices[r]-prices[l]
-                maxVa = max(profit,maxVa)
-                # maxVa = diff
+                current_Profit = prices[r] - prices[l]
+                mp = max(current_Profit,mp)
             else:
-                l = r
+                l=r
             r+=1
-        return maxVa                
+        return mp
+
+
+
+
+
+    '''
+    Technique : Brute force
+    Time Complexity : O(n*2)
+    Space Complexity :O(1)
+    Remarks: Not an efficent method it will work
+    '''
+    def SellStock_Brt(self, prices: list[int])->list[int]:
+        print("Brute force Approach")
+        res = 0 
+        for i in range(len(prices)):
+            buy = prices[i]
+            for j in range(i+1,len(prices)):
+                sell = prices[j]
+                res = max(res,sell - buy)
+        return res
+
+
+
+
+
 
 
 if __name__ =='__main__':
     cCall = solution()
     prices = [7,1,5,3,6,4]
-    val = cCall.SellStock(prices)
+    val = cCall.SellStock_Tp(prices)
     print(val)
 
