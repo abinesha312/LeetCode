197818901bb86055d9b2525b9efbe8c77623606f
Abinesha312 <Abinesha312@gmail.com>
2025-03-26 14:23:02 -0500

    Two Sum two with binary search | TC - O(n.log n) SC- O(1)

--- Arrays & Hashing/167_TwoPointer.py
+++ Arrays & Hashing/167_TwoPointer.py
@@ -1,3 +1,9 @@
+'''
+probelm Two sum II in two approachs 
+    1) Two Pointer Approach 
+    2) Binary Search with | Time Complexity - o(n.log.n) & Space Complexity O(1) constant space 
+'''
+
 class TwoPointer:
     '''
     Two Pointer Technique in the soted arrya 
@@ -13,10 +19,29 @@ class TwoPointer:
             else:
                 return [l+1,r+1]
         return []
+    
+    def twosum_bs(self, nums:list[int], target: int) -> list[int]:
+        print("Executing the Binary Search approach")
+        for i in range(len(nums)-1):
+            l,r = i+1, len(nums)-1
+            temp = target - nums[i]
+            while l<=r:
+                mid = l + (r - l)//2
+                if nums[mid] == temp:
+                    return [i+1,mid+1]
+                elif nums[mid] < temp:
+                    l = mid+1
+                else:
+                    r = mid-1
+        return []
+
+
+
+
 
 if __name__ =="__main__":
     classcall = TwoPointer()
-    inputnums = [[2,7,11,15]]
-    target = 9
-    result = classcall.twoSum(inputnums,target)
+    inputnums = [1,2,3,4]
+    target = 3
+    result = classcall.twosum_bs(inputnums,target)
     print(result)
