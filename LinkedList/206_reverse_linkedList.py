6f4ba0b8a92cc837e39f9cc9f8669b7fab730f8a
Abinesha312 <Abinesha312@gmail.com>
2025-03-26 17:49:21 -0500

    LinkedList With Recursive Call

--- LinkedList/206_reverse_linkedList.py
+++ LinkedList/206_reverse_linkedList.py
@@ -1,12 +1,13 @@
 from typing import Optional
 
-'''
+
+class solution:
+    '''
     Technique : Brute force
     Time Complexity : O(n*2)
     Space Complexity :O(1)
     Remarks: Not an efficent method it will work
-'''
-class solution:
+    '''
     def reverseLinkedList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         curr = head
         prev = None
@@ -16,6 +17,26 @@ class solution:
             prev = curr
             curr = t
         return prev
+    
+
+    '''
+    Technique : Brute force
+    Time Complexity : O(n*2)
+    Space Complexity :O(1)
+    Remarks: Not an efficent method it will work
+    '''    
+    def reverseLinkedListRC(self, head:Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        
+        nhead = head
+        if head.next:
+            nhead = self.reverseLinkedListRC(head.next)
+            head.next.next = head
+        head.next = None
+
+
+
 
 
 if __name__ =="__main__":
